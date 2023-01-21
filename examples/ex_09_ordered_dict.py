9c650148f30246fc3cdf1aa455da71f89b8392e1
ariannedee <ariannedee@gmail.com>
2023-01-21 14:38:39 -0800

    Add OrderedDict examples

--- examples/ex_09_ordered_dict.py
+++ examples/ex_09_ordered_dict.py
@@ -1,1 +1,59 @@
+# # collections.OrderedDict
 
+from collections import OrderedDict
+
+# ## Create
+# ### Empty dict
+
+a = OrderedDict()
+a
+
+# ### From a dict
+
+b = OrderedDict({1: 'a', 2: 'b'})
+b
+
+# ### From a sequence
+# Each item in the sequence must be another sequence with length 2
+
+c = OrderedDict([['a', 'apple'], {'b', 'banana'}, ('c', 'cherry')])
+c
+
+# ## Use like a dictionary
+# `OrderedDict` has all the same methods as `dict`.
+
+b[0] = 'z'
+b
+
+for i in range(4):
+    print(b.get(i, 'Not found'))
+
+# ## Iteration order
+# When iterating, it retains memory of insertion order
+
+for key, value in b.items():
+    print(f'{key}: {value}')
+
+# ## Pop item
+#     Additional method to pop the last 
+
+# ## Reorder items
+# `.move_to_end(key, last=True)` will move the item with `key` either to the end (`last=True`) or the front (`last=False`).
+
+b.move_to_end(1)
+b
+
+b.move_to_end(0, last=False)
+b
+
+for key, value in b.items():
+    print(f'{key}: {value}')
+
+# ## When to use OrderedDict over dict
+# `dict` is has been ordered since Python3.6.
+#
+# You should still use `OrderedDict` to:
+# - Clearly state that the order is important
+# - Use the `.move_to_end()` method
+# - Test dictionary equality based on order
+# - Maintain backwards compatibility with Python < 3.6
