4b15125593815918a985c1730636785f6824df1e
ariannedee <ariannedee@gmail.com>
2023-01-24 21:16:25 -0800

    Initialize file read

--- project/survey_analysis.py
+++ project/survey_analysis.py
@@ -7,7 +7,14 @@ all_langs = ['Python', 'Java', 'JavaScript', 'TypeScript', 'PHP', 'C', 'C++', 'C
 
 # ## Load data
 
+# +
+import csv
 
+with open('data/Programming language survey.csv') as file:
+    reader = csv.DictReader(file)
+    for line in reader:
+        print(line)
+# -
 
 # ## Find number of languages known
 # Print `"{# known by class} / {# in list} languages known by this class (as %)"`.
